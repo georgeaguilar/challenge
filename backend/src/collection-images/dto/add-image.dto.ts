f47150ce55ae321ec5fc0c02af29d029d3be7b56
Jorge Aguilar <georgeaguilar11@hotmail.com>
2026-05-30 11:35:41 -0600

    feat: add add-image and add-tag DTOs for collection images

---
+++ backend/src/collection-images/dto/add-image.dto.ts
@@ -0,0 +1,7 @@
+export class AddImageDto {
+  nasaId: string;
+  title: string;
+  url: string;
+  date?: string;
+  description?: string;
+}
---
+++ backend/src/collection-images/dto/add-tag.dto.ts
@@ -0,0 +1,3 @@
+export class AddTagDto {
+  name: string;
+}
