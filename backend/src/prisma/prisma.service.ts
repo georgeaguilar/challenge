d96610bd868abe6c2ff2317e15c9d5529ab7b90c
Jorge Aguilar <georgeaguilar11@hotmail.com>
2026-05-30 00:03:42 -0600

    feat: add PrismaService and PrismaModule with NestJS dependency injection

---
+++ backend/src/prisma/prisma.service.ts
@@ -0,0 +1,42 @@
+import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
+import { PrismaClient } from '../../generated/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+
+@Injectable()
+export class PrismaService implements OnModuleInit, OnModuleDestroy {
+  private readonly client: InstanceType<typeof PrismaClient>;
+
+  constructor() {
+    const adapter = new PrismaPg(process.env.DATABASE_URL!);
+    this.client = new PrismaClient({ adapter });
+  }
+
+  async onModuleInit() {
+    await this.client.$connect();
+  }
+
+  async onModuleDestroy() {
+    await this.client.$disconnect();
+  }
+
+  get user() {
+    return this.client.user;
+  }
+  get collection() {
+    return this.client.collection;
+  }
+  get collectionImage() {
+    return this.client.collectionImage;
+  }
+  get tag() {
+    return this.client.tag;
+  }
+  get imageTag() {
+    return this.client.imageTag;
+  }
+  get $transaction(): typeof this.client.$transaction {
+    return this.client.$transaction.bind(
+      this.client,
+    ) as typeof this.client.$transaction;
+  }
+}
