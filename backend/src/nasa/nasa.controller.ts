417e157804875ab1da4fea247344c5d6959eaffc
Jorge Aguilar <georgeaguilar11@hotmail.com>
2026-05-30 11:28:56 -0600

    feat: add NasaController with GET /nasa/search and /nasa/images/:nasaId

---
+++ backend/src/nasa/nasa.controller.ts
@@ -0,0 +1,21 @@
+import { Controller, Get, Param, Query, UseGuards } from '@nestjs/common';
+import { QueryBus } from '@nestjs/cqrs';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { SearchImagesQuery } from './queries/search-images/search-images.query';
+import { GetImageQuery } from './queries/get-image/get-image.query';
+
+@UseGuards(JwtAuthGuard)
+@Controller('nasa')
+export class NasaController {
+  constructor(private readonly queryBus: QueryBus) {}
+
+  @Get('search')
+  search(@Query('q') q: string, @Query('page') page = 1) {
+    return this.queryBus.execute(new SearchImagesQuery(q, Number(page)));
+  }
+
+  @Get('images/:nasaId')
+  getImage(@Param('nasaId') nasaId: string) {
+    return this.queryBus.execute(new GetImageQuery(nasaId));
+  }
+}
